c489233a5b6dfcd6680df638750022ba4e43c45e
skreebydba <skreebydba@users.noreply.github.com>
2019-08-23 10:20:10 -0500

    Added conditional logic
    Added logic to check for existence of the container, backup path, backup file, and dockerlogin.

--- CreateSQLContainer.ps1
+++ CreateSQLContainer.ps1
@@ -1,13 +1,22 @@
-﻿$container = "sql2019rc";
+﻿$container = "sql2019rc2";
+$port = 1435
 $instance = "localhost,1433";
 $database = "WideWorldImporters";
 $dockerlogin = "dockerlogin";
 
-docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=P@ssw0rd1" -p 1433:1433 --name $container -d mcr.microsoft.com/mssql/server:2019-latest;
+$containerexists = docker ps -f "name=$container" --format "{{.Names}}";
 
-$cred = Get-Credential;
+$containerexists;
 
-$agentquery = @"
+if($container -ne $containerexists)
+{
+    Write-Host "$container does not exist" -ForegroundColor Cyan;
+
+    docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=P@ssw0rd1" -p $port`:1433 --name $container -d mcr.microsoft.com/mssql/server:2019-latest;
+
+    $cred = Get-Credential;
+
+    $agentquery = @"
 EXEC sp_configure 'Show Advanced Options',1;
 RECONFIGURE;
 EXEC sp_configure 'Agent XPs', 1;
@@ -16,20 +25,21 @@ EXEC sp_configure 'Show Advanced Options',0;
 RECONFIGURE;
 "@;
 
-Invoke-DbaQuery -SqlInstance $instance -SqlCredential $cred -Database master -Query $agentquery;
+    Invoke-DbaQuery -SqlInstance $instance -SqlCredential $cred -Database master -Query $agentquery;
 
-Invoke-DbaQuery -SqlInstance $instance -SqlCredential $cred -Database master -Query "SELECT @@VERSION";
+    Invoke-DbaQuery -SqlInstance $instance -SqlCredential $cred -Database master -Query "SELECT @@VERSION";
 
-$exists = Get-DbaLogin -SqlInstance $instance -SqlCredential $cred -Login $dockerlogin;
-if(!$exists)
-{
-    $securePassword = Read-Host "Input password" -AsSecureString
-    New-DbaLogin -SqlInstance $instance -SqlCredential $cred -Login $dockerlogin -SecurePassword $securePassword -PasswordPolicyEnforced;
-    Set-DbaLogin -SqlInstance $instance -SqlCredential $cred -Login $dockerlogin -AddRole sysadmin;
+    $exists = Get-DbaLogin -SqlInstance $instance -SqlCredential $cred -Login $dockerlogin;
+    if(!$exists)
+    {
+        $securePassword = Read-Host "Input password" -AsSecureString
+        New-DbaLogin -SqlInstance $instance -SqlCredential $cred -Login $dockerlogin -SecurePassword $securePassword -PasswordPolicyEnforced;
+        Set-DbaLogin -SqlInstance $instance -SqlCredential $cred -Login $dockerlogin -AddRole sysadmin;
+    }
 }
 
-docker exec -it sql2019rc mkdir /var/opt/mssql/backup
-
+docker exec -it sql2019rc mkdir -p /var/opt/mssql/backup
+ 
 docker cp "C:\backup\WideWorldImporters\WideWorldImporters-Full.bak" sql2019rc:/var/opt/mssql/backup
 
 $dockercred = Get-Credential;
@@ -43,5 +53,5 @@ $filestructure = @{
 #/var/opt/mssql/data/WWI.ldf
 #/var/opt/mssql/data/WWI.ndf
 
-Restore-DbaDatabase -SqlInstance $instance -DatabaseName $database -SqlCredential $dockercred -Path "/var/opt/mssql/backup/WideWorldImporters-Full.bak" -FileMapping $filestructure;
+Restore-DbaDatabase -SqlInstance $instance -DatabaseName $database -SqlCredential $dockercred -Path "/var/opt/mssql/backup/WideWorldImporters-Full.bak" -FileMapping $filestructure -WithReplace;
 
