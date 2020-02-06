430136bb6871e93b3bec15fb6a9da783b3e7f653
skreebydba <skreebydba@users.noreply.github.com>
2020-02-06 13:20:25 -0600

    Delete EnableWindowsContainers.ps1

--- EnableWindowsContainers.ps1
+++
@@ -1,8 +0,0 @@
-﻿Enable-WindowsOptionalFeature -Online -FeatureName containers –All
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V –All
-Disable-WindowsOptionalFeature -FeatureName containers
-Disable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V 
-
-#docker build -t sql2008:1.0 .
-#Switch from Windows to Linux containers
-#& $Env:ProgramFiles\Docker\Docker\DockerCli.exe -SwitchDaemon
