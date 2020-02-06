ffeb6a3623fd4e78146793a47053626a46aa4d8b
skreebydba <skreebydba@users.noreply.github.com>
2020-02-06 13:20:04 -0600

    Delete CreateContainerWithVolume.ps1

--- CreateContainerWithVolume.ps1
+++
@@ -1,7 +0,0 @@
-﻿$container = "sql2019rc3";
-$port = '1437:1433'
-$instance = "localhost,1433";
-$database = "WideWorldImporters";
-$dockerlogin = "dockerlogin";
-
-docker run -v d:/backup:/backup -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=P@ssw0rd1" -p $port --name $container -d mcr.microsoft.com/mssql/server:2019-latest #;
